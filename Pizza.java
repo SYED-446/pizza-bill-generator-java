7b17b19621c8cdec825fda21f996843fa773f680
SYED-446 <149327860+SYED-446@users.noreply.github.com>
2026-03-08 13:02:38 +0530

    Added Pizza.java

---
+++ Pizza.java
@@ -0,0 +1,18 @@
+class Pizza {
+
+    private String name;
+    private int price;
+
+    public Pizza(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
